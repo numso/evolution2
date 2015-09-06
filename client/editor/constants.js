a5b83b7b2c5c811be18c28bf44a95ab5fcc53455
Dallin Osmun <dosmun@gmail.com>
2015-09-05 21:07:34 -0600

    version 1

--- client/editor/constants.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-
-export var TILE_SIZE = 40
-export var WIDTH = 20
-export var HEIGHT = 15
-
-export var TILE_BLOCK = 0
-export var TILE_TRAMPOLINE = 1
--- client/game/music.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-
-import {Howl} from 'howler'
-
-export var bg = new Howl({
-  urls: ['snd/dream-culture.mp3'],
-  volume: 0.5,
-  loop: true
-})
